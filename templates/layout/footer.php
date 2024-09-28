6d3e65e6b9b02f867760df2d371ceba8369d386d
gregory1175 <gredmyr@gmail.com>
2024-09-28 19:33:16 +0300

    update footer

--- templates/layout/footer.php
+++ templates/layout/footer.php
@@ -1,35 +1,26 @@
 <footer class="container pt-4 my-md-5 pt-md-5 border-top">
     <div class="row">
       <div class="col-12 col-md">
-        <small class="d-block mb-3 text-body-secondary">© 2017–2024</small>
-      </div>
-      <div class="col-6 col-md">
-        <h5>Фичи</h5>
-        <ul class="list-unstyled text-small">
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Ссылка</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Ссылка</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Ссылка</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Ссылка</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Ссылка</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Ссылка</a></li>
-        </ul>
-      </div>
-      <div class="col-6 col-md">
-        <h5>Ресурсы</h5>
-        <ul class="list-unstyled text-small">
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Ссылка</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Ссылка</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Ссылка</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Ссылка</a></li>
-        </ul>
+        <small class="d-block mb-3 text-body-secondary">© 2024</small>
       </div>
       <div class="col-6 col-md">
         <h5>О нас</h5>
         <ul class="list-unstyled text-small">
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Ссылка</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Ссылка</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Ссылка</a></li>
-          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Ссылка</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Профессиональная политика сообщества</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Условия & конфиданциальность</a></li>
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Центр поддержки</a></li>
+        </ul>
+      </div>
+      <div class="col-6 col-md">
+        <h5>Доступность</h5>
+        <ul class="list-unstyled text-small">
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Мобильная версия</a></li>
+        </ul>
+      </div>
+      <div class="col-6 col-md">
+        <h5>Дополнительно</h5>
+        <ul class="list-unstyled text-small">
+          <li class="mb-1"><a class="link-secondary text-decoration-none" href="#">Малый бизнес</a></li>
         </ul>
       </div>
     </div>
